768167016b47d23d735f74cc9eeb4834ecf2fd01
Erick Martins da Silva <erickmartinsservice@gmail.com>
2024-07-29 22:09:42 -0300

    front end structure

--- frontend-enviroment/my-app/src/login/screen/request/Sender.js
+++ frontend-enviroment/my-app/src/login/screen/request/Sender.js
@@ -1,19 +1,27 @@
 import axios from "axios"
 
-function send_request(){
+const msgError = "some fields is empty!"
+
+export function send_request(){
     const getContent = () =>{
+        const empty = (document.getElementById("fragment-email").value == "") && (document.getElementById("fragment-password").value == "")
+        if (empty){return msgError}
+
         return(
             {
                 "type":"login",
-                "email": document.getElementById("fragment-email"),
-                "password": document.getElementById("fragment-password")
+                "email": document.getElementById("fragment-email").value,
+                "password": document.getElementById("fragment-password").value
             }
         )
     }
+    const jsonReturn = getContent()
+    if (jsonReturn == msgError){
+        return msgError
+    }
+
     const awnser = axios.post(
-        "http://127.0.0.1:5000",
-        getContent()
+        "http://127.0.0.1:5000/lobby",
+        jsonReturn
     )
 }
-
-export default send_request();
