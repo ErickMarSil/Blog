acb7294c560a78d0bf999798286034f94fc1ec3f
Erick Martins da Silva <erickmartinsservice@gmail.com>
2024-07-29 21:24:18 -0300

    front end structure

--- frontend-enviroment/my-app/src/index.js
+++ frontend-enviroment/my-app/src/index.js
@@ -1,18 +1,12 @@
 import React from 'react';
-import { useEffect } from 'react';
-// import { createConnection } from 
 import ReactDOM from 'react-dom/client';
-import Login from './lobby_assests/login_screen';
+import App from './login/screen/App';
 import reportWebVitals from './reportWebVitals';
-
-const Informations = async() =>{
-  
-}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Login />
+    <App />
   </React.StrictMode>
 );
 
