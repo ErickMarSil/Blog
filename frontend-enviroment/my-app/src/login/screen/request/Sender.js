a3c8b41042d0f62afa847e7ef660f4baf981464c
Erick Martins da Silva <erickmartinsservice@gmail.com>
2024-07-30 22:37:58 -0300

    front end and flask api changes

--- frontend-enviroment/my-app/src/login/screen/request/Sender.js
+++ frontend-enviroment/my-app/src/login/screen/request/Sender.js
@@ -1,27 +1,21 @@
-import axios from "axios"
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
-const msgError = "some fields is empty!"
-
-export function send_request(){
-    const getContent = () =>{
-        const empty = (document.getElementById("fragment-email").value == "") && (document.getElementById("fragment-password").value == "")
-        if (empty){return msgError}
-
-        return(
-            {
-                "type":"login",
-                "email": document.getElementById("fragment-email").value,
-                "password": document.getElementById("fragment-password").value
-            }
+export function SendRequestLogin(){
+    const [userInformations, setUserInformations] = useState({});
+    const getInformations = async () =>{
+        const infroamtionList = axios.post(
+            "http://127.0.0.1:5000",
+            () => {
+                return(
+                    {
+                    "type": "login", 
+                    "email": document.getElementById("fragment-email").value, 
+                    "password":document.getElementById("fragment-password").value
+                });}
         )
     }
-    const jsonReturn = getContent()
-    if (jsonReturn == msgError){
-        return msgError
-    }
-
-    const awnser = axios.post(
-        "http://127.0.0.1:5000/lobby",
-        jsonReturn
-    )
+    return(
+        ""
+    );
 }
