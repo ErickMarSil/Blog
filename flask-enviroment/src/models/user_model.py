2078d6cf5dd4c33cd7d7072945048c90bd5cd078
Erick Martins da Silva <erickmartinsservice@gmail.com>
2024-08-04 13:03:08 -0300

    temp

--- flask-enviroment/src/models/user_model.py
+++ flask-enviroment/src/models/user_model.py
@@ -1,4 +1,15 @@
 from src.models import db
 
-class User(db.Model):
-    pass
+class Bases(db.Model):
+    def validate_login_credentials(content):
+        # take all the user informations
+        # validate password in the hash module
+        # if corrects return all the content
+        # otherwise return none content
+        pass
+
+    def insert_signin_credentiasl(content):
+        # insert all the informations
+        # set up the password hash + saly + cost
+        # return the informations less password
+        pass
