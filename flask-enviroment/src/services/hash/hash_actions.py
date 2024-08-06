fd37f481602397d30619c4519ff4441a312ec9e6
Erick Martins da Silva <erickmartinsservice@gmail.com>
2024-08-05 23:03:28 -0300

    poop solved

--- flask-enviroment/src/services/hash/hash_actions.py
+++ flask-enviroment/src/services/hash/hash_actions.py
@@ -1,9 +1,9 @@
 import bcrypt
 
-def generate_hash(ori_password, salt=""):
+def generate_hash(ori_password:str, salt=""):
     if salt == "": salt = generate_salt()
 
-    hash_obj = bcrypt.hashpw(password=ori_password, salt=salt)
+    hash_obj = bcrypt.hashpw(password=ori_password.encode(), salt=salt)
     return {"hash":hash_obj.hexdigest(), "salt":salt}
 
 def generate_salt() -> str:
