f37a9773de689d0c6efa8a50f2626c46b701badb
Erick Martins da Silva <erickmartinsservice@gmail.com>
2024-08-07 21:57:47 -0300

    spliting obligations

--- flask-enviroment/src/services/validation/signin_validation.py
+++ flask-enviroment/src/services/validation/signin_validation.py
@@ -1,30 +1,47 @@
 from flask import request
 from src.controller.database.user_controller import Actions
+import datetime
 action = Actions()
 
 def validate_information(content):
-    credentials_validation = action.insert_signin_credentiasl(content=content)
-    if credentials_validation:
+
+    valids = validate_structure(content=content)
+    if valids["valid"] == False:
         return {
-            "valid":True,
-            "redirect":{
-                "doRedirect":True,
-                "routeHttp":"/login"
-            },
-            "message":"Cadastro feito com sucesso, vá até o login"
-        }
-    else:
-        return {
-            "valid":False,
-            "redirect":{
-                "doRedirect":False,
-                "routeHttp":""
-            },
-            "message":"Cadastro não realizado, tente novamente"
+            "token":"",
+            "wrongData":valids,
+            "message":"Algum campo excedeu o limite ou esta formatado erroniamente"
         }
 
-def validate_structure(content):
-    retJson ={
-        "first_name": (),
-        "last_name":()
+    credentials_validation = action.insert_signin_credentiasl(content=content)
+
+    if credentials_validation == True: message = "Cadastro realizado com sucesso"
+    else: message = "Falha ao fazer cadastro, tente novamente"
+
+    return {
+        "valid":credentials_validation,
+        "redirect":True,
+        "message": message
     }
+
+def validate_structure(content):
+    try:
+        birthValid = (datetime.datetime.strptime(content["birth_date"],"%d/%m/%y"))
+    except:
+        birthValid = False
+    retJson ={
+        "first_name":(any(not i.isalpha for i in content["first_name"]) and len(content["first_name"]) <= 50),
+        "last_name":(any(not i.isalpha for i in content["last_name"]) and len(content["last_name"]) <= 50),
+        "email":(len(content["email"]) <= 30),
+        "birth_date":birthValid,
+        "nickname":(len(content["nickname"]) <= 16),
+        "valid":False
+    }
+    retJson["valid"] = (
+        retJson["first_name"] == True and 
+        retJson["last_name"] == True and
+        retJson["email"] == True and
+        retJson["birth_date"] == True and
+        retJson["nickname"] == True
+    )
+    return retJson
