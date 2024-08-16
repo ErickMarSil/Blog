37130f0feee330b2bdace7b0b59562bcb3eb0a54
Erick Martins da Silva <erickmartinsservice@gmail.com>
2024-08-15 23:01:00 -0300

    added:cors in the flask application

--- react-enviroment/api-frontend/src/context/LoginContext.jsx
+++ react-enviroment/api-frontend/src/context/LoginContext.jsx
@@ -1,14 +1,27 @@
-import { createContext } from "react";
+import axios from "axios";
+import { createContext, useState } from "react";
 
 export const LoginContext = createContext({LoginContextProvider})
 
 export function LoginContextProvider({children}){
-    async function Login_Request(data){
-        console.log(data);
-    }
+
+    const [userInfos, setUserInfos] = useState({})
+
+        const Login_Request = async ({ email, password }) => {
+            return axios.post(
+                "http://127.0.0.1:5000/login",
+                { email, password }
+            ).then(
+                awnser => {
+                    return awnser.data;
+                }
+            ).catch(
+                console.log("wasn´t able to connect with the server")
+            )
+        } 
 
     return(
-        <LoginContext.Provider value={{Login_Request}}>
+        <LoginContext.Provider value={{Login_Request, userInfos}}>
             {children}
         </LoginContext.Provider>
     )
--- react-enviroment/api-frontend/src/context/SigninContext.jsx
+++ react-enviroment/api-frontend/src/context/SigninContext.jsx
@@ -4,8 +4,19 @@ import { createContext } from "react";
 export const SigninContext = createContext({SigninContextProvider})
 
 export function SigninContextProvider({ children }){
-    async function Signin_Request(data){
-        axios.post
+    async function Signin_Request({ first_name, last_name, email, password, birth_date, nickname }){
+        const awnser = await axios.post(
+            "http://127.0.0.1:5000/signin",
+            {
+                first_name,
+                last_name,
+                email,
+                password,
+                birth_date,
+                nickname
+            }
+        );
+        console.log(awnser);
     }
 
     return(
